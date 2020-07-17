5385853732541b1e057dbdda363a9d00d8ff1cde
felipemuner <felipe.muner@gmail.com>
2020-07-17 10:47:22 +0700

    set up teachers table

--- pages/teacher/index.vue
+++ pages/teacher/index.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
+    {{selected}}
     <h1>home page teacher</h1>
-    {{teachers}}
-    {{name}}
+    <template>
+      <v-data-table
+        v-model="selected"
+        :headers="headers"
+        :items="teachers"
+        item-key="teacherID"
+        show-select
+        class="elevation-1"
+      >
+      </v-data-table>
+    </template>
   </div>
 </template>
 
@@ -13,7 +23,21 @@ import { mapActions } from "vuex";
 export default {
   data() {
     return {
-      name: "teacher"
+      name: "teacher",
+      selected: [],
+      headers: [
+        { text: 'ID', value: 'teacherID' },
+        { text: 'Email', value: 'email' },
+        {
+          text: 'Name',
+          align: 'start',
+          value: 'name',
+        },
+        { text: 'Birthday', value: 'dateOfBirth' },
+        { text: 'Phone', value: 'phone' },
+        { text: 'TA', value: 'isTeacherAssistant' },
+        { text: 'Active', value: 'active' },
+      ],        
     };
   },
   computed: {
