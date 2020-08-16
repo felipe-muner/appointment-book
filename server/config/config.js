37a0f5b70410b0be70ad6aa81475507fe4186112
felipemuner <felipe.muner@gmail.com>
2020-08-16 21:38:29 +0700

    set up sequelize option to return float instead string for decimals

--- server/config/config.js
+++ server/config/config.js
@@ -5,20 +5,23 @@ module.exports = {
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.HOST,
-    dialect: "mysql"
+    dialect: "mysql",
+    dialectOptions: { decimalNumbers: true }
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.HOST,
-    dialect: "mysql"
+    dialect: "mysql",
+    dialectOptions: { decimalNumbers: true }
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.HOST,
-    dialect: "mysql"
+    dialect: "mysql",
+    dialectOptions: { decimalNumbers: true }
   }
 };
--- server/utils/utils.js
+++ server/utils/utils.js
@@ -9,5 +9,12 @@ module.exports = {
   },
   extractTime: dt => {
     return moment(dt).format("HH:mm");
-  }
+  },
+  getDaysInMonth(firstDay, lastDay) {
+    let allDays = [];
+    for (let d = firstDay; d <= lastDay; d.setDate(d.getDate() + 1)) {
+      allDays.push(new Date(d));
+    }
+  },
+  drawArrayShitf() {}
 };
